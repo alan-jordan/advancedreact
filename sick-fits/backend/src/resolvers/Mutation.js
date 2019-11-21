e1d58661e51f634a96c9bc4f4469ba278544b672
Alan Jordan <alanpjordan@gmail.com>
2019-11-21 21:02:00 +1300

    Lesson 13 done.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const Mutations = {
+  createDog(parent, args, context, info) {
+    // create a dog!
+    global.dogs = global.dogs || [];
+    console.log(args)
+    const newDog =  { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
