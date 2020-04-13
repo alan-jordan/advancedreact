574594ecf51f8e00752d339a840fa0baaa754edf
Alan Jordan <alanpjordan@gmail.com>
2020-04-14 11:50:13 +1200

    Completed lesson 14 - Adds some queries and mutations for Items.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,12 +1,25 @@
 const Mutations = {
-  createDog(parent, args, context, info) {
-    // create a dog!
-    global.dogs = global.dogs || [];
-    console.log(args)
-    const newDog =  { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parentrs, args, ctx, info) {
+    // TODO : Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
   }
+  // createDog(parent, args, context, info) {
+  //   // create a dog!
+  //   global.dogs = global.dogs || [];
+  //   console.log(args);
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
 };
 
 module.exports = Mutations;
