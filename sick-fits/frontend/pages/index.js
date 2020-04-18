10f0b97447a851290b65e832c7b4d654bfd5b031
Alan Jordan <alanpjordan@gmail.com>
2020-04-19 11:42:46 +1200

    Did a lesson.

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
-
-const Home = props => (
+import Items from "../components/Items";
+const Home = (props) => (
   <div>
-    <p>Hey!</p>
+    <Items />
   </div>
 );
 
